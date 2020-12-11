8edbe4811a472cd041ea5d51d04782f354ad1764
John Stableford <john.stableford@catch.com.au>
2020-12-11 19:55:39 +1100

    chore: fix storybook summary page

--- packages/web-components/src/stories/summary.stories.tsx
+++ packages/web-components/src/stories/summary.stories.tsx
@@ -7,6 +7,7 @@ import {
   UnorderedList,
   ListItem,
 } from "@bootleg-rust/lib-design-system";
+import { StorybookWrapper } from "./shared";
 import atomicDesignProcess from "./atomic-design-process.png";
 
 // eslint-disable-next-line import/no-default-export
@@ -16,40 +17,42 @@ export default {
 
 export function AtomicDesign() {
   return (
-    <Div>
-      <H1>
-        Atomic design (
-        <Anchor href="https://atomicdesign.bradfrost.com/chapter-2/#the-atomic-design-methodology">
-          https://atomicdesign.bradfrost.com/chapter-2/#the-atomic-design-methodology
-        </Anchor>
-        )
-        <Anchor href="https://atomicdesign.bradfrost.com/chapter-2/#the-atomic-design-methodology">
-          https://atomicdesign.bradfrost.com/chapter-2/#the-atomic-design-methodology
-        </Anchor>
-        https://bradfrost.com/blog/post/atomic-web-design/
-      </H1>
-      <Paragraph>
-        Atomic design is a methodology composed of five distinct stages working
-        together to create interface design systems in a more deliberate and
-        hierarchical manner.
-      </Paragraph>
-      <br />
-      <Paragraph>The five stages of atomic design are:</Paragraph>
-      <br />
-      <UnorderedList>
-        <ListItem>Atoms</ListItem>
-        <ListItem>Molecules</ListItem>
-        <ListItem>Organisms</ListItem>
-        <ListItem>Templates</ListItem>
-        <ListItem>Pages</ListItem>
-      </UnorderedList>
-      <br />
+    <StorybookWrapper>
+      <Div>
+        <H1>
+          Atomic design (
+          <Anchor href="https://atomicdesign.bradfrost.com/chapter-2/#the-atomic-design-methodology">
+            https://atomicdesign.bradfrost.com/chapter-2/#the-atomic-design-methodology
+          </Anchor>
+          )
+          <Anchor href="https://atomicdesign.bradfrost.com/chapter-2/#the-atomic-design-methodology">
+            https://atomicdesign.bradfrost.com/chapter-2/#the-atomic-design-methodology
+          </Anchor>
+          https://bradfrost.com/blog/post/atomic-web-design/
+        </H1>
+        <Paragraph>
+          Atomic design is a methodology composed of five distinct stages
+          working together to create interface design systems in a more
+          deliberate and hierarchical manner.
+        </Paragraph>
+        <br />
+        <Paragraph>The five stages of atomic design are:</Paragraph>
+        <br />
+        <UnorderedList>
+          <ListItem>Atoms</ListItem>
+          <ListItem>Molecules</ListItem>
+          <ListItem>Organisms</ListItem>
+          <ListItem>Templates</ListItem>
+          <ListItem>Pages</ListItem>
+        </UnorderedList>
+        <br />
 
-      <img
-        src={atomicDesignProcess}
-        alt="atomic design process including atoms, molecules, organisms, templates and pages"
-      />
-    </Div>
+        <img
+          src={atomicDesignProcess}
+          alt="atomic design process including atoms, molecules, organisms, templates and pages"
+        />
+      </Div>
+    </StorybookWrapper>
   );
 }
 
