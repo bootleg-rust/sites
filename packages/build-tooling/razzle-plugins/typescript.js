42b544228b40e4cf51a58fee92081075551c5696
John Stableford <john.stableford@catch.com.au>
2020-12-12 02:32:01 +1100

    chore: update to razzle canary

--- packages/build-tooling/razzle-plugins/typescript.js
+++ packages/build-tooling/razzle-plugins/typescript.js
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 const makeLoaderFinder = require("razzle-dev-utils/makeLoaderFinder");
 const babelLoaderFinder = makeLoaderFinder("babel-loader");
-const ForkTSCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+// const ForkTSCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
 
 const defaultOptions = {
   include: [],
@@ -16,14 +16,6 @@ function modifyWebpackConfig({
 }) {
   const options = { ...defaultOptions, ...pluginOptions };
 
-  webpackConfig.resolve.extensions = [
-    ...webpackConfig.resolve.extensions,
-    ".ts",
-    ".tsx",
-  ];
-
-  webpackConfig.plugins.push(new ForkTSCheckerWebpackPlugin());
-
   // Safely locate Babel loader in Razzle's webpack internals
   const babelLoader = webpackConfig.module.rules.find((rule) =>
     babelLoaderFinder(rule),
@@ -33,25 +25,34 @@ function modifyWebpackConfig({
       `'babel-loader' was erased from config, we need it to define typescript options`,
     );
   }
-
-  babelLoader.test = [babelLoader.test, /\.tsx?$/];
   babelLoader.include = [...babelLoader.include, ...options.include];
-  babelLoader.use[0].options = {
-    babelrc: false,
-    cacheDirectory: true,
-  };
 
-  // Client: ts optimization on development
-  if (target === "web" && dev) {
-    // As suggested by Microsoft's Outlook team, these optimizations crank up Webpack x TypeScript perf.
-    // https://medium.com/@kenneth_chau/speeding-up-webpack-typescript-incremental-builds-by-7x-3912ba4c1d15
-    webpackConfig.output.pathinfo = false;
-    webpackConfig.optimization = {
-      removeAvailableModules: false,
-      removeEmptyChunks: false,
-      splitChunks: false,
-    };
-  }
+  // webpackConfig.resolve.extensions = [
+  //   ...webpackConfig.resolve.extensions,
+  //   ".ts",
+  //   ".tsx",
+  // ];
+
+  // webpackConfig.plugins.push(new ForkTSCheckerWebpackPlugin());
+
+  // babelLoader.test = [babelLoader.test, /\.tsx?$/];
+  // babelLoader.include = [...babelLoader.include, ...options.include];
+  // babelLoader.use[0].options = {
+  //   babelrc: false,
+  //   cacheDirectory: true,
+  // };
+
+  // // Client: ts optimization on development
+  // if (target === "web" && dev) {
+  //   // As suggested by Microsoft's Outlook team, these optimizations crank up Webpack x TypeScript perf.
+  //   // https://medium.com/@kenneth_chau/speeding-up-webpack-typescript-incremental-builds-by-7x-3912ba4c1d15
+  //   webpackConfig.output.pathinfo = false;
+  //   webpackConfig.optimization = {
+  //     removeAvailableModules: false,
+  //     removeEmptyChunks: false,
+  //     splitChunks: false,
+  //   };
+  // }
 
   return webpackConfig;
 }
