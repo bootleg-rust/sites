06d0ff83a15bf2b9ac24a498342634a45c0084ad
pseudo-su <stableford.john@gmail.com>
2020-12-12 23:32:21 +1100

    chore: clean up file categorization

--- packages/build-tooling/razzle-plugins/cacheable-assets.js
+++ packages/build-tooling/razzle-plugins/cacheable-assets.js
@@ -2,8 +2,32 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 const path = require("path");
 const { WebpackManifestPlugin } = require("webpack-manifest-plugin");
+const { fileMatchesTemplate } = require("./utils/file-matches-template");
 
 // const defaultOptions = {};
+
+function fileMatchesAnyRegexp(file, regExpressions) {
+  return regExpressions.some((re) => re.test(file.path));
+}
+
+/**
+ * Determine whether the config of the webpack loader associated with this file
+ * contains "buildhash", "contenthash" or "hash". Webpack output config on loaders
+ * can be specified as a template string or function.
+ *
+ * @param {string|function} webpackOutputFilename
+ * @param {any} file
+ */
+function webpackLoaderOutputContainsHash(webpackOutputFilename, file) {
+  const filename =
+    typeof webpackFilenameOption == "function"
+      ? webpackOutputFilename(file)
+      : webpackOutputFilename;
+
+  const containsHash = /\[(build|content)?hash/.test(filename);
+
+  return containsHash;
+}
 
 function modifyWebpackConfig({
   env: { target, dev },
@@ -12,6 +36,26 @@ function modifyWebpackConfig({
   options: { pluginOptions, razzleOptions, webpackOptions },
   paths,
 }) {
+  // TODO: allow passing in extra file categorizers as `pluginOptions`
+  const fileCategorizers = [
+    {
+      test: webpackOptions.urlLoaderTest,
+      outputName: webpackOptions.urlLoaderOutputName,
+    },
+    {
+      test: webpackOptions.cssTest,
+      outputName: webpackOptions.cssOutputFilename,
+    },
+    {
+      test: webpackOptions.jsTest,
+      outputName: webpackOptions.jsOutputFilename,
+    },
+    {
+      exclude: webpackOptions.fileLoaderExclude,
+      outputName: webpackOptions.fileLoaderOutputName,
+    },
+  ];
+
   const fileName = path.join(paths.appBuild, "cacheable-assets.json");
   const assetPlugin = new WebpackManifestPlugin({
     fileName,
@@ -28,52 +72,42 @@ function modifyWebpackConfig({
         }
       };
 
-      // console.log(files[0]);
       files.forEach((file) => {
-        // console.log(file);
-        if (
-          !webpackOptions.fileLoaderExclude.every((re) => re.test(file.path))
-        ) {
-          // console.log({ fileLoader: webpackOptions.fileLoaderOutputName });
-          const name =
-            typeof webpackOptions.fileLoaderOutputName == "function"
-              ? webpackOptions.fileLoaderOutputName(file)
-              : webpackOptions.fileLoaderOutputName;
-          // console.log(name);
-          const containsHash = /\[(build|content)?hash/.test(name);
+        if (file.name.startsWith("..")) {
+          // Files that start with ".." will live outside of the public/
+          // folder and therefore can't/shouldn't be accessed.
+          return;
+        }
 
-          setFileAs(file, { containsHash });
-        } else if (
-          webpackOptions.urlLoaderTest.some((re) => re.test(file.path))
-        ) {
-          // console.log({ urlLoader: webpackOptions.urlLoaderOutputName });
-          const containsHash = /\[(build|content)?hash/.test(
-            typeof webpackOptions.urlLoaderOutputName == "function"
-              ? webpackOptions.urlLoaderOutputName(file)
-              : webpackOptions.urlLoaderOutputName,
-          );
+        const fileCategorized = fileCategorizers.some(
+          ({ test, exclude, outputName }) => {
+            const passesTest =
+              test != null ? fileMatchesAnyRegexp(file, test) : true;
 
-          setFileAs(file, { containsHash });
-        } else if (webpackOptions.cssTest.some((re) => re.test(file.path))) {
-          // console.log({ cssOutput: webpackOptions.cssOutputFilename });
-          const containsHash = /\[(build|content)?hash/.test(
-            typeof webpackOptions.cssOutputFilename == "function"
-              ? webpackOptions.cssOutputFilename(file)
-              : webpackOptions.cssOutputFilename,
-          );
+            const passesExclude =
+              exclude != null ? !fileMatchesAnyRegexp(file, exclude) : true;
 
-          setFileAs(file, { containsHash });
-        } else if (webpackOptions.jsTest.some((re) => re.test(file.path))) {
-          // console.log({ jsOutput: webpackOptions.jsOutputFilename });
-          const containsHash = /\[(build|content)?hash/.test(
-            typeof webpackOptions.jsOutputFilename == "function"
-              ? webpackOptions.jsOutputFilename(file)
-              : webpackOptions.jsOutputFilename,
-          );
+            const fileMatches =
+              passesTest &&
+              passesExclude &&
+              fileMatchesTemplate(file.path, outputName);
 
-          setFileAs(file, { containsHash });
-        } else {
-          // console.log('UNKNOWN!!', file);
+            if (fileMatches) {
+              const containsHash = webpackLoaderOutputContainsHash(
+                outputName,
+                file,
+              );
+
+              setFileAs(file, { containsHash });
+            }
+
+            return fileMatches;
+          },
+        );
+
+        if (!fileCategorized) {
+          // TODO: allow "strict" vs "lazy" mode here where we can only use
+          // regex on the filename to guess if a file contains a hash in it.
           setFileAs(file, { containsHash: false });
         }
       });
