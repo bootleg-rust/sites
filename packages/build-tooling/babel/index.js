ca95c44d24a0e3b1864e1493b3605e8f58bf555e
John Stableford <john.stableford@catch.com.au>
2020-12-14 01:54:13 +1100

    chore: add some commands

--- packages/build-tooling/babel/index.js
+++ packages/build-tooling/babel/index.js
@@ -1,45 +1,9 @@
 module.exports = {
   presets: [
-    "@babel/preset-react",
-    [
-      "@babel/preset-env",
-      {
-        targets: {
-          node: "current",
-        },
-        useBuiltIns: "entry",
-        corejs: 3,
-      },
-    ],
-    "@babel/preset-typescript",
     "razzle/babel",
   ],
+
   plugins: [
-    "@babel/plugin-proposal-object-rest-spread",
-    "@babel/plugin-proposal-class-properties",
-    "@babel/plugin-proposal-numeric-separator",
-    "@babel/plugin-proposal-export-default-from",
-    "@babel/plugin-proposal-export-namespace-from",
-    "@babel/plugin-proposal-async-generator-functions",
-    "@babel/plugin-proposal-optional-chaining",
-    "babel-plugin-styled-components",
+    require.resolve("babel-plugin-styled-components"),
   ],
-  env: {
-    test: {
-      presets: [
-        "@babel/preset-react",
-        ["@babel/preset-env", { targets: { node: "current" } }],
-        "@babel/preset-typescript",
-      ],
-      plugins: [
-        "@babel/plugin-proposal-object-rest-spread",
-        "@babel/plugin-proposal-class-properties",
-        "@babel/plugin-proposal-export-default-from",
-        "@babel/plugin-proposal-export-namespace-from",
-        "@babel/plugin-proposal-async-generator-functions",
-        "@babel/plugin-proposal-optional-chaining",
-        // "babel-plugin-styled-components",
-      ],
-    },
-  },
 };
