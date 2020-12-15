bed01929e598209faf678b8528888882ed69e820
John Stableford <john.stableford@catch.com.au>
2020-12-16 00:40:32 +1100

    chore: add back commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,40 @@
+// TODO: use this in a git hook
+// commitlint -E HUSKY_GIT_PARAMS
+module.exports = {
+  rules: {
+    "body-leading-blank": [1, "always"],
+    "footer-leading-blank": [1, "always"],
+    "header-max-length": [2, "always", 100],
+    "scope-case": [2, "always", "lower-case"],
+    "subject-case": [
+      2,
+      "never",
+      ["sentence-case", "start-case", "pascal-case", "upper-case"],
+    ],
+    "subject-empty": [2, "never"],
+    "subject-full-stop": [2, "never", "."],
+    "type-case": [2, "always", "lower-case"],
+    "type-empty": [2, "never"],
+    "type-enum": [
+      2,
+      "always",
+      [
+        "wip",
+        "build",
+        "chore",
+        "ci",
+        "docs",
+        "feat",
+        "fix",
+        "perf",
+        "refactor",
+        "revert",
+        "style",
+        "test",
+        "migration",
+        "type",
+        "lint",
+      ],
+    ],
+  },
+};
